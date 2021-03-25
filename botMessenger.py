536e4436488af12ebb065b0300b2e7baae1d460a
codeinlife10 <codeinlife10@gmail.com>
2021-03-25 15:55:11 +0700

    deploy

--- botMessenger.py
+++ botMessenger.py
@@ -6,8 +6,8 @@ from pymessenger.bot import Bot
 import os
 
 app = Flask(__name__)
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
 bot = Bot(ACCESS_TOKEN)
 
 #We will receive messages that Facebook sends our bot at this endpoint 
